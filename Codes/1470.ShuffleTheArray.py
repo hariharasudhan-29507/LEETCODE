533dd4b74f516a465ccb9e96354b9f4f3d3c5a44
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-04-13 22:57:10 +0530

    Update 1470.ShuffleTheArray.py

--- Codes/1470.ShuffleTheArray.py
+++ Codes/1470.ShuffleTheArray.py
@@ -5,8 +5,10 @@ class Solution(object):
         :type n: int
         :rtype: List[int]
         """
-        ans = [0] * (2 * n)
+        res_arr=[0 for x in range(2*n)]
+        count=0
         for i in range(n):
-            ans[2 * i] = nums[i]
-            ans[2 * i + 1] = nums[i + n]
-        return ans
+            res_arr[count] = nums[i]
+            res_arr[count+1] = nums[i + n]
+            count=count+2
+        return res_arr
