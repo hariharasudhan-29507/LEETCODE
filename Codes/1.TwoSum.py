594e445b027929eee1b276efbce652463b32d5bf
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-04-14 17:45:07 +0530

    Refactor twoSum method to remove type hints

--- Codes/1.TwoSum.py
+++ Codes/1.TwoSum.py
@@ -1,5 +1,5 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums, target):
         res=[]
         for i in range(0,len(nums)):
             for j in range(i+1,len(nums)):
