8ea8918abe628753c3fa3cff52e86470e837c036
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-04-14 18:33:04 +0530

    Update 1929.ConcatenationOfArray.py

--- Codes/1929.ConcatenationOfArray.py
+++ Codes/1929.ConcatenationOfArray.py
@@ -1,12 +1,20 @@
+# Author Hariharasudhan A
 class Solution(object):
     def getConcatenation(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
+        # the array needs to be concatenated with the same array 
+        '''
+        this does the same thing 
+        * def getConcatenation(self, nums):
+              return nums + nums
+        * def getConcatenation(self, nums):
+              return nums * 2
+           
+        '''
+        # traverse the given array and store it another
         nums1=[]
         for i in range (len(nums)):
             nums1.append(nums[i])
+        # append the copy array elements 
         for i in nums1:
             nums.append(i)
         return nums
