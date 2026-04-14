4acba5646f127d6326b2fe84bd42b0d38276b42c
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-04-14 19:03:59 +0530

    Update 22.GenerateParenthesis.py

--- Codes/22.GenerateParenthesis.py
+++ Codes/22.GenerateParenthesis.py
@@ -1,20 +1,25 @@
+#Author - Hariharasudhan A
 class Solution(object):
     def generateParenthesis(self, n):
-        res = []
-        def dfs(left, right, s):
-            if len(s) == n * 2:
-                res.append(s)
-                return 
-
-            if left < n:
-                dfs(left + 1, right, s + '(')
-
-            if right < left:
-                dfs(left, right + 1, s + ')')
+        stack=[]# empty stack
+        res=[]# empty result array
         
-        
-        dfs(0, 0, "") 
+        def backTracking(countO,countC):
+            '''
+            countO - count of "("
+            countC - count of ")"
+            '''
+            if countO==countC==n:
+                res.append("".join(stack)) # joins the elements in the stack as a single string and we are inserting in result array
+
+            # main condition for placing a ")" is when countC < countO ,i.e ((( then we can place a ) , if ()) then we cant place a )
+            if countO < n: # for open parenthesis "(
+                stack.append("(")
+                backTracking(countO+1,countC)
+                stack.pop() #c cleaning the stack
+            if countC < countO: # for close parenthesis ")"
+                stack.append(")")
+                backTracking(countO,countC+1)
+                stack.pop() # cleaning the stack
+        backTracking(0,0)
         return res
-
-s = Solution()
-print(s.generateParenthesis(3))
