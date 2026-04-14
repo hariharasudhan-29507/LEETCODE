1ca22bdfddec6409f5a3baf2908d4efb6d6b1404
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-04-14 18:16:34 +0530

    Update 1470.ShuffleTheArray.py

--- Codes/1470.ShuffleTheArray.py
+++ Codes/1470.ShuffleTheArray.py
@@ -1,14 +1,21 @@
+#Author - Hariharasudhan A
 class Solution(object):
     def shuffle(self, nums, n):
-        """
-        :type nums: List[int]
-        :type n: int
-        :rtype: List[int]
-        """
+        '''
+        initialise a array with '0' with 2*n size 
+        because 'n' is partition on nums to split 'nums' into parts of 'n'
+        '''
         res_arr=[0 for x in range(2*n)]
         count=0
-        for i in range(n):
+        '''
+        index of nums goes as 0,1,2,3,4,5 the partition comes as [0,1,2] | [3,4,5]
+        if n=3 
+        i=0 | i+n = 3
+        i=1 | i+n = 4
+        i=2 | i+n = 5
+        '''
+        for i in range(n): # shuffle them accordingly as i , i+n
             res_arr[count] = nums[i]
             res_arr[count+1] = nums[i + n]
-            count=count+2
+            count=count+2 
         return res_arr
