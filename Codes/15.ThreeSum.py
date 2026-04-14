999cb7ec85d0748b4d60189e3155cc84b1adac87
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-04-14 18:21:06 +0530

    Update 15.ThreeSum.py

--- Codes/15.ThreeSum.py
+++ Codes/15.ThreeSum.py
@@ -1,25 +1,27 @@
+# Author - Hariharasudhan A
 class Solution(object):
     def threeSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
+
         res=[]
         target=0
-        nums.sort()
-        for i ,val in enumerate(nums):
-            if i>0 and val==nums[i-1]:
+        nums.sort() # sorting them because we cant have duplicate sets 
+        for i ,val in enumerate(nums): # traversing values along its position
+            if i>0 and val==nums[i-1]: # if the current value is the same as its i-1 value on array
                 continue
-            l,r = i+1 ,len(nums)-1
+            l,r = i+1 ,len(nums)-1 # keeping left and right valus on the array 
 
-            while l<r:
-                sum=val+nums[l]+nums[r]
-                if sum>target:
-                    r-=1
-                elif sum < target:
+            while l<r: 
+                sum=val+nums[l]+nums[r] 
+                '''
+                traversing through every value on array using the outer for loop
+                here we will find the sum up to target condtion using while  loop 
+                '''
+                if sum>target: # since we sorted the array , the largest elements are on the end of array
+                    r-=1 
+                elif sum < target: # smallest elements are on the start 
                     l+=1
                 else:
-                    res.append([val,nums[l],nums[r]])
+                    res.append([val,nums[l],nums[r]]) # sum up to target values 
                     l+=1
             
         return res
