704d8efcc3d6063e983bef96242735742e2c40b2
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-04-14 18:11:46 +0530

    Update 14.Longest Common Prefix.py

--- Codes/14.Longest Common Prefix.py
+++ Codes/14.Longest Common Prefix.py
@@ -1,17 +1,23 @@
+#Author - Hariharasudhan A
 class Solution(object):
     def longestCommonPrefix(self, strs):
-        strs.sort()
-        s=""
+        strs.sort() # ['flight', 'flow', 'flower'] 
+        '''
+        it is enough to compare with 'flow' as 'flow' is in 'flower'
+        sorting the array helps us to arrange the substrings 
+        '''
+        s="" 
         lens=len(strs)
         i=0
+        # traverse through each and every character parallely 
         while i < len(strs[0]):
             if strs[0][i] == strs[lens-1][i]:
                 s+=strs[0][i]
             else:
                 break
             i=i+1
-        return s
+        return s # return the longest common prefix on array 
         
 s=Solution()
 strs=["flower","flight","flow"]
-s.longestCommonPrefix(strs)
+print(s.longestCommonPrefix(strs))
