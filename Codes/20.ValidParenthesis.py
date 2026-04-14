92e83b630ebbf544b2276a25455f994bd64ea83e
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-04-14 18:39:42 +0530

    Update 20.ValidParenthesis.py

--- Codes/20.ValidParenthesis.py
+++ Codes/20.ValidParenthesis.py
@@ -1,17 +1,21 @@
 
 class Solution(object):
     def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
+        # create all possible types 
         mapping = {')': '(', '}': '{', ']': '['}
-        stack = []
-
+        stack = [] # empty stack
+        
         for char in s:
-            if char in mapping.values():
-                stack.append(char)
-            elif char in mapping:
-                if not stack or mapping[char] != stack.pop():
+            if char in mapping.values(): # only for opening brackets 
+                stack.append(char) # push every fresh element
+            elif char in mapping: # only for closing brackets 
+                if not stack or mapping[char] != stack.pop(): 
+                    '''
+                    if stack doesnot exits / key doesnot match with pair then false 
+                    '''
                     return False
-        return not stack
+        '''
+        if stack is empty - True // key : pairs matched and every pushed element is popped
+        if not - False // key : pairs not matched and every pushed element is not popped
+        '''
+        return not stack 
