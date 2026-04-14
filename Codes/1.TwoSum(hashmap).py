3dc9cf9ade48d1d98f036ca172fb34aebe8e4909
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-04-14 17:44:37 +0530

    Update 1.TwoSum(hashmap).py

--- Codes/1.TwoSum(hashmap).py
+++ Codes/1.TwoSum(hashmap).py
@@ -1,12 +1,13 @@
+#author-Hariharasudhan A
 from collections import defaultdict
 class Solution(object):
     def twoSum(self, nums, target):
 
-        map=defaultdict(int)
+        map=defaultdict(int) # initialize the hashmap using placeholders they are integerss
 
-        for i,val in enumerate(nums):
-            difference=target - val 
+        for i,val in enumerate(nums): # enumerate helps us to traverse through values on array along respective positions
+            difference=target - val #hash function h(x)=target - x 
             if difference in map:
-                return [map[difference],i]
+                return [map[difference],i] # if it is not unique then the already existing value and the new value are the values that sum up to target 
             else:
-                map[val]=i
+                map[val]=i # insert the value in hashmap if it is unique
