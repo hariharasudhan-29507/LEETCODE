cb772b0cef30d7ea905ddb71032177cbd610cd97
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-04-14 18:23:27 +0530

    Update 167.TwoSum II.py

--- Codes/167.TwoSum II.py
+++ Codes/167.TwoSum II.py
@@ -1,17 +1,14 @@
+# Author - Hariharasudhan A
 class Solution(object):
     def twoSum(self, numbers, target):
-        """
-        :type numbers: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+        # keeping the left, right values on the array 
         l,r=0,len(numbers)-1
         
         while l<r:
-            sum=numbers[l]+numbers[r]
-            if sum>target:
+            sum=numbers[l]+numbers[r] # checking sum up to values 
+            if sum>target: # if sum is 12 , target is 9 ,then we can conclude that the values are large enough to omit
                 r-=1
-            elif sum<target:
+            elif sum<target: # if sum is 3 , target is 9 ,then we can conclude that the values are small enough to omit
                 l+=1
             else:
                 return [l+1,r+1]
