cce627f526e78b2f6da0fa4d80e3497c790e6569
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-04-14 19:04:39 +0530

    Update 242.ValidAnagram(Counter).py

--- Codes/242.ValidAnagram(Counter).py
+++ Codes/242.ValidAnagram(Counter).py
@@ -1,4 +1,8 @@
+#Author - Hariharasudhan
 from collections import Counter
+''' 
+collection is a hashmap 
+'''
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         return Counter(s) == Counter(t)    
