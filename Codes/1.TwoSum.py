b5585c45cf4654bd48672f336304ad8a3705c3fe
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-04-14 18:01:25 +0530

    Update 1.TwoSum.py

--- Codes/1.TwoSum.py
+++ Codes/1.TwoSum.py
@@ -1,12 +1,14 @@
+#brute force
 class Solution:
     def twoSum(self, nums, target):
         res=[]
-        for i in range(0,len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i]+nums[j]==target:
-                    res.append(i)
+        # iterate through every element and add them and compare with target 
+        for i in range(0,len(nums)): # num 1 iteration
+            for j in range(i+1,len(nums)): # num 2 iteration
+                if nums[i]+nums[j]==target: # if num1+num2 = target 
+                    res.append(i) # insert the indices in to the array
                     res.append(j)
-                    return res
+                    return res # return the indices
                     break            
 nums=[2,7,11,15]
 target=9
