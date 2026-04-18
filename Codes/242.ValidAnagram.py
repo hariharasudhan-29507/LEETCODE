f1e2acd659fc6566d8d33f989bb6fca25e9b293f
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-04-18 18:07:16 +0530

    Update 242.ValidAnagram.py

--- Codes/242.ValidAnagram.py
+++ Codes/242.ValidAnagram.py
@@ -1,18 +1,14 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        if len(s) != len(t):
+    
+        if len(s) != len(t): # if length varies then there is no chance on having same set of alphabets
             return False
-        countS,countT = {},{}
-        for i in range(len(s)):
+        countS,countT = {},{} 
+        for i in range(len(s)): # counting every single alphabet and storing it as value to key pair , alphabet : count
             countS[s[i]] =1+countS.get(s[i],0)
             countT[t[i]] =1+countT.get(t[i],0)
-
-        for c in countS:
+        # get() returns default value if it doesnot exist in hashmap , instead of throwing a error 
+        for c in countS: # checking the counts 
             if countS[c] != countT.get(c,0):
                 return False
         return True
