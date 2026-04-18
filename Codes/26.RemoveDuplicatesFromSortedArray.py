ebb7c8fa7b876462e07dafd5f5711ef3b3e2ca15
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-04-18 18:09:11 +0530

    Update 26.RemoveDuplicatesFromSortedArray.py

--- Codes/26.RemoveDuplicatesFromSortedArray.py
+++ Codes/26.RemoveDuplicatesFromSortedArray.py
@@ -1,6 +1,7 @@
+#Author - Hariharasudhan A
 class Solution(object):
     	def removeDuplicates(self, nums):
-            nums[:] = sorted(set(nums))
+            nums[:] = sorted(set(nums)) # set can't have duplicates
             return len(nums)
 s=Solution()
 s.removeDuplicates([1,1,2])
