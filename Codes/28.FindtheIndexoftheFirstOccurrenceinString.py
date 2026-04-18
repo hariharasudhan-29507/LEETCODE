e8aeefbcc245ab62a42d4f7d6f93b8fad51754c1
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-04-18 18:24:37 +0530

    Update 28.FindtheIndexoftheFirstOccurrenceinString.py

--- Codes/28.FindtheIndexoftheFirstOccurrenceinString.py
+++ Codes/28.FindtheIndexoftheFirstOccurrenceinString.py
@@ -1,9 +1,10 @@
+# Author - Hariharasudhan A
 class Solution(object):
     def strStr(self, haystack, needle):
-        lens=len(needle)
-        for i in range(0,len(haystack)):
-           if  haystack[i:i+lens]==needle:
-                return i
+        lens=len(needle) # len of substring
+        for i in range(0,len(haystack)):# iterate till length of string
+           if  haystack[i:i+lens]==needle: # if substring present in string 
+                return i # return index
         return -1
 s= Solution()
 s.strStr("sadbutsad","sad")
