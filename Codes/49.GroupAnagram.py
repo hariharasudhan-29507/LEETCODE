41a1ba19f834eb128da1fed8439e1cd708a4fde7
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-04-18 18:37:54 +0530

    Update 49.GroupAnagram.py

--- Codes/49.GroupAnagram.py
+++ Codes/49.GroupAnagram.py
@@ -1,14 +1,17 @@
+# Author - Hariharasudhan A
 from collections import defaultdict
 class Solution(object):
     def groupAnagrams(self, strs):
-        
+        '''
+        defaultdict is a collection that helps us to initialize a dictionary with whatever datatype
+        '''
         anagram=defaultdict(list)
         res=[]
         
         for s in strs:
-            sorted_strs=tuple(sorted(s))
-            anagram[sorted_strs].append(s)
-        for value in anagram.values():
+            sorted_strs=tuple(sorted(s)) # keys need to be immutable , so we use tuples
+            anagram[sorted_strs].append(s) # alphabet tuples are paired with its corresponding words
+        for value in anagram.values(): # only add the paired sets 
             res.append(value)
 
         return res
