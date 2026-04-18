b71c254345123ba20908946bfad9141dbe29c774
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-04-18 18:31:43 +0530

    Update 485.MaxConsecutiveOnes.py

--- Codes/485.MaxConsecutiveOnes.py
+++ Codes/485.MaxConsecutiveOnes.py
@@ -1,15 +1,12 @@
+# Author - Hariharasudhan A
 class Solution:
     def findMaxConsecutiveOnes(self, nums) :
         max_ones=0
         count=0
-        for num in (nums):
-            if num==1:
-                count=count+1
+        for num in (nums): # traverse every element in nums
+            if num==1: # ones may be ordered in any with zero as separator 
+                count=count+1 # count number of ones
                 max_ones= max(max_ones,count) #max_ones = max_ones if max_ones > count else count
             else:
                 count=0
-        return max_ones
-                
-            
-
-        
+        return max_ones 
