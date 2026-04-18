ca6e5f1ba0aa4c0b50a7640a8ecbd34abcf6d9d9
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-04-18 18:29:19 +0530

    Update 35.SearchInsertPosition.py

--- Codes/35.SearchInsertPosition.py
+++ Codes/35.SearchInsertPosition.py
@@ -1,17 +1,13 @@
+# Author - Hariharasudhan A
 class Solution(object):
     def fetchIndex(self,nums,target):
-        for i,val in enumerate(nums):
-            if val==target:
+        for i,val in enumerate(nums): # traverse through values along with index 
+            if val==target: # if target locked then return index
                 return i
     def searchInsert(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        if target in nums:
+        if target in nums: # if target already exists then fetch index
             return self.fetchIndex(nums,target)
-        else:
+        else: # if not then add it - sort it - then fetch index
             nums.append(target)
             nums.sort()
             return self.fetchIndex(nums,target)
