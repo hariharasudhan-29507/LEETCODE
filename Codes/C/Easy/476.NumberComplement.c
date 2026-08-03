884f68120b5735924ba9909fa6a160af3de2fa17
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-08-03 12:13:00 +0530

    Update 476.NumberComplement.c

--- Codes/C/Easy/476.NumberComplement.c
+++ Codes/C/Easy/476.NumberComplement.c
@@ -1,18 +1,62 @@
-int* countBits(int n, int* returnSize) {
-    
-    *returnSize = n + 1;
-    int *result = malloc((n + 1) * sizeof(int));
-    for (int i = 0; i <= n; i++) {
-        int temp = i;
-        int count = 0;
-
-        while (temp > 0) {
-            count += temp % 2;
-            temp /= 2;
+class Solution {
+public:
+    void reverse(char* bin){
+        int start = 0;
+        int end = strlen(bin) - 1;
+        char  temp;
+        while (start < end) {
+            temp = bin[start];
+            bin[start] = bin[end];
+            bin[end] = temp;
+            start++;
+            end--;
         }
-
-        result[i] = count;
     }
 
-    return result;
-}
+    void ToBinaryString(int num, char* bin_str) {
+        int len;
+        if (num == 0) {
+            strcpy(bin_str, "0");
+            return;
+        }
+        len = 0;
+        while (num > 0) {
+            bin_str[len++] = (num % 2) + '0';
+            num /= 2;
+        }
+        bin_str[len] = '\0';
+        reverse(bin_str);
+    }
+
+    int ToNumFromString(char* bin_str) {
+        int res = 0;
+        int len = strlen(bin_str);
+        int i;                              
+        for (i = 0; i < len; i++) {
+            res = res * 2 + (bin_str[i] - '0');
+        }
+        return res;
+    }
+
+    int findComplement(int num) {
+        char bin_str[64];
+        int len;
+        char comp_str[64];
+        int i;                              
+   
+        ToBinaryString(num, bin_str);
+   
+        len = strlen(bin_str);
+        for (i = 0; i < len; i++) {
+            if (bin_str[i] == '1') {
+                comp_str[i] = '0';
+            } else {
+                comp_str[i] = '1';
+            }
+        }
+        comp_str[len] = '\0';
+   
+        return ToNumFromString(comp_str);
+        }
+
+};
