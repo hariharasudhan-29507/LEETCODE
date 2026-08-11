9edfd3f50f7f0b67516452e2a727be460be0c915
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-08-11 15:24:48 +0530

    max consecutive onescpp

---
+++ DSA-Quest/ArrayI/C++/Q3.MaxConsecutiveOnes.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int count = 0 , res = 0 ;
+        for(int num : nums){
+            if(num == 1){
+                count++;
+                res = max(res,count);
+            }
+            
+            else{
+                count = 0;
+            }
+            }
+
+        return res;
+    }
+};
