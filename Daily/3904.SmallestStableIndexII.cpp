5130d1f902ee7864f60c2b6cf505a3255e3d2d05
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-09-06 22:59:11 +0530

    Create 3904.SmallestStableIndexII.cpp

---
+++ Daily/3904.SmallestStableIndexII.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> suf(n);
+        suf[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; --i) {
+            suf[i] = min(suf[i + 1], nums[i]);
+        }
+        int pre = nums[0];
+        for (int i = 0; i < n; ++i) {
+            if (i > 0) pre = max(pre, nums[i]);
+            if (pre - suf[i] <= k) return i;
+        }
+        return -1;
+    }
+};
